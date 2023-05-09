e9eb24b89f3fe5ad68ac7b5b60ee076a0e61458e
Davood <da.mirzaee@gmail.com>
2023-05-09 16:29:23 +0330

    Insert input from parent to the child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  post = [
+    {
+      title: "Neat Tree" ,
+      imageUrl: "assets/tree.jpeg",
+      username: "@nature",
+      content: "saw this yummy tree on my way"
+    },
+    {
+      title: "Big mountain" ,
+      imageUrl: "assets/mountain.jpeg",
+      username: "@mountain",
+      content: "I climb a mountain like you walk on flat ground"
+    },
+    {
+      title: "bike rider" ,
+      imageUrl: "assets/biking.jpeg",
+      username: "@bike1456",
+      content: "I ride bike then go to hike to see a mike on the ride."
+    }
+  ]
 }
